24a4bd733ba055af5b1eddf2dd0d2842450f8277
rajat <rc425@njit.edu>
2020-03-03 17:20:11 -0500

    updated DB connection

--- Db_connection.php
+++ Db_connection.php
@@ -2,13 +2,33 @@
 $servername = "rajat-VirtualBox"; 
 $username = "rajat"; 
 $password = "Soumya!07"; 
+$db = "IT_490";
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+ini_set('display_errors' , 1);
+
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	exit();
+}
 
 //Create Connection 
-$conn = new mysqli($servername, $username, $password); 
+$conn = new mysqli($servername, $username, $password, $db); 
 
 //Check Connection 
 if ($conn->connect_error){
     die("Connection failed:" . $conn->connect_error);
 }
 echo "Connected Successfully";
+
+	
+$s = "select * from Drug_Info";
+($t = mysqli_query( $db, $s))  or die( mysqli_error($db));
+while ( $r = mysqli_fetch_array($t,MYSQLI_ASSOC) ) {
+    $drug_id = $r[ "drug_id" ];
+    $drug_name = $r["drug_name" ];
+
+    echo $drug_name;
+    echo $drug_id;
+}
 ?>

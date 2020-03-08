893dacb2aa8c6245318196ede2123cd53320a36b
richie124 <33379635+richie124@users.noreply.github.com>
2020-03-08 14:13:52 -0400

    Update consumer_localhost.php

--- frontEnd/consumer_localhost.php
+++ frontEnd/consumer_localhost.php
@@ -1,17 +1,5 @@
-<!DOCTYPE HTML>
-
 <?php
-<html>
-	<head>
-		<title>Richard Ching</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
-		<link rel="stylesheet" href="styles.css">
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	</head>
-	<body>
-		<div class="container">
+
 
 require_once __DIR__ . '/../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
@@ -46,8 +34,3 @@ $connection->close();
 
 
 ?>
-			<br>
-			<a href="formpage.php">Back to formpage.php</a>
-		</div>
-	</body>
-</html>

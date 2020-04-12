ae46e1a6e362ec15033728b53f1d4d0b2c380576
richie124 <richard1ching@gmail.com>
2020-04-12 19:40:35 -0400

    logs

--- frontEnd/publisher.php
+++ frontEnd/publisher.php
@@ -5,13 +5,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('25.132.137.114', 5672, 'test', 'testHost');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+$channel->queue_declare('rmqQueue', false, false, false, false);
 
 $msg = new AMQPMessage('Hello World!');
-$channel->basic_publish($msg, '', 'hello');
+$channel->basic_publish($msg, '', 'rmqQueue');
 
 echo " [x] Sent 'Hello World!'\n";
 
